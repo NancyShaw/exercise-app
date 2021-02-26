833acdbc30165e0572afe4a447bef7cef3262dab
Nancy Shaw <78286574+NancyShaw@users.noreply.github.com>
2021-02-25 23:12:55 -0500

    Creating vue client from vue starter project -Adding views with placeholder headings -Creating a Nav component so the navbar will look the same throughout the app -Adding views to the router and linking in Nav to enable navigation

---
+++ client/src/views/ActivityDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+        <form class="box">
+            <div class="field">
+                <label class="label">Type of Exercise</label>
+                <div>
+                    <input class="input" type="text" placeholder="Curling" />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Number of Minutes Spent</label>
+                <div>
+                    <input class="input" type="number" placeholder="45" />
+                </div>
+            </div>
+
+            <button class="button is-primary">Submit</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
