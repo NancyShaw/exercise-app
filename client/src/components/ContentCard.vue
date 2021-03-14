74d2aa59ab2184ff815e255c373fa9f43b0dff51
Nancy Shaw <78286574+NancyShaw@users.noreply.github.com>
2021-03-13 22:11:44 -0500

    Post/delete functionality on user wall -Currently sort of works

--- client/src/components/ContentCard.vue
+++ client/src/components/ContentCard.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="card">
       <div class="card-content">
+          <button class="delete is-pulled-right" @click.prevent="$emit('delete')"></button>
+
           <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img src="../assets/triangles.png" alt="placeholder for user photo">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img :src="post.user.pic" :alt="post.user.name" />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">{{post.user.name}}</p>
+              <p class="subtitle is-6">{{post.user.handle}}</p>
+            </div>
+          </div>
+      </div>
+      <div class="card-image">
+        <figure class="image">
+          <img :src="post.src" :alt="post.alt" />
         </figure>
       </div>
-      <div class="media-content">
-        <p class="title is-4">Poster/Sharer's actual name</p>
-        <p class="subtitle is-6">Fun name/social media handle</p>
-      </div>
-    </div>
-      </div>
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder">
-    </figure>
-  </div>
-  <div class="card-content">
+      <div class="card-content">
 
-    <div class="content">
-       I reached my Goals today!!!
-       <br />
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-    </div>
+          <div class="content">
+            {{post.caption}}
+            <br />
+            <time :datetime="post.time">{{post.time}}</time>
+          </div>
+      </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
+  props: {
+      post: Object
+  }
 
-}
+};
 </script>
 
 <style>
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="container">
+  <div id="my-wall">
     <div class="columns">
       <div class="column is-one-quarter">
-          <FriendsList :friends="friends" />
-      </div>
-        
           <div class="content-item">
-            
-            <myContentCard />
-          
-          <div class="column is-one-quarter"></div>
+              <FriendsList :friends="friends" />
+          </div>
+      </div>
+      <div class="column">
+
+        <div class="content-item">
+            <ContentCreation :newPost="newPost" @add="addPost" />
         </div>
+
+        <div class="content-item" v-for="(post, i) in posts" :key="i">
+          <ContentCard :post="posts" @delete="deletePost(i)" />
+        </div>
+      </div>
+      <div class="column is-one-quarter">
+          <div class="content-item">
+            <ContentCard :post="newPost" />
+          </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import myContentCard from "../components/ContentCard";
+import Vue from "vue";
+import ContentCard from "../components/ContentCard.vue";
+import ContentCreation from "../components/ContentCreation.vue";
 import FriendsList from '../components/FriendsList.vue';
-export default {
+import { GetMyPosts } from "../models/Posts";
+
+export default Vue.extend({
   data: ()=> ({
         friends: [
             {
@@ -41,17 +55,35 @@ export default {
                 src: "https://bulma.io/images/placeholders/128x128.png",
                 alt: "Placeholder image"
             }
-        ]
+        ],
+        newPost: {
+            user: {
+            }
+        },
+        posts: []
     }),
-  components: {
-    myContentCard,
-    FriendsList
-  }  
-}
+    mounted() {
+        this.posts = GetMyPosts();
+    },
+    components: {
+      ContentCard,
+      ContentCreation,
+      FriendsList
+    },
+    methods: {
+        addPost() {
+            this.posts.unshift(this.newPost);
+            this.newPost = { user: {} };
+        },
+        deletePost(i) {
+          this.posts.splice(i, 1);
+        }
+    }  
+})
 </script>
 
 <style>
-.container {
+.content-item {
     margin-top: 10px;
     margin-bottom: 10px;
 }
