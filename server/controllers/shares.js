f0471834624e533fa99b787982e39e5608b95bda
Nancy Shaw <78286574+NancyShaw@users.noreply.github.com>
2021-04-16 19:26:52 -0400

    Improvements to shares functionality

--- server/controllers/shares.js
+++ server/controllers/shares.js
@@ -13,23 +13,12 @@ const app = express.Router();
                 res.send( model.GetWall( req.user.handle) );
             }            
         }  )
-        .get('/:share_id',(req,res)=> {
-            if(+req.params.share_id) {
-                //what's my use-case for this? do I have access control?
-                res.send( model.Get(req.params.share_id) )
-            } else {
-                res.send( model.GetWall( req.params.share_id) );
-            }
-        })
         
         .post('/', (req, res)=> { 
             req.body.handle = req.user.handle;
             res.send( model.Add(req.body) );
         })
-        .patch('/:share_id', (req,res)=> res.send(
-            model.Update( req.params.share_id, req.body)
-        ))
-        .delete('/share_id', (req,res)=> res.send(model.Delete(req.params.share_id) ) )
-        
 
+        .delete('/:share_id', (req,res)=> res.send(model.Delete(req.params.share_id) ) )
+        
 module.exports = app;
--- server/models/shares.js
+++ server/models/shares.js
@@ -4,28 +4,31 @@ const users = require("./users");
 
 const shares_list = [ 
     { 
-    src: "https://bulma.io/images/placeholders/1280x960.png",
-    alt: "Placeholder image",
-    caption: "Going plain clothes",
-    time: Date(),
-    handle: "@ItsTheNose",
-    isPublic: true,
+        src: "https://bulma.io/images/placeholders/1280x960.png",
+        alt: "Placeholder image",
+        caption: "Going plain clothes",
+        time: Date(),
+        handle: "@ItsTheNose",
+        isPublic: true,
+        id: 1,
     },
     { 
-    src: "https://bulma.io/images/placeholders/1280x960.png",
-    alt: "Placeholder image",
-    caption: "Testing samples with Igor",
-    time: Date(),
-    handle: "@Forensics",
-    isPublic: true, 
+        src: "https://bulma.io/images/placeholders/1280x960.png",
+        alt: "Placeholder image",
+        caption: "Testing samples with Igor",
+        time: Date(),
+        handle: "@Forensics",
+        isPublic: true,
+        id: 4, 
     },
     { 
-    src: "https://bulma.io/images/placeholders/1280x960.png",
-    alt: "Placeholder image",
-    caption: "At a crime scene.",
-    time: Date(),
-    handle: "@Forensics",
-    isPublic: true,
+        src: "https://bulma.io/images/placeholders/1280x960.png",
+        alt: "Placeholder image",
+        caption: "At a crime scene.",
+        time: Date(),
+        handle: "@Forensics",
+        isPublic: true,
+        id: 6,
     },
     { 
         src: "https://bulma.io/images/placeholders/1280x960.png",
@@ -34,6 +37,7 @@ const shares_list = [
         time: Date(),
         handle: "@ImBatty",
         isPublic: true,
+        id: 7,
     }
 ];
 
@@ -43,49 +47,36 @@ const listWithOwner = ()=> shares_list.map((x, i) => ({
 }) );
 
 const filterOutUserInfo = ((shares)=> {
-    return shares.forEach(s=> s.user=null);
+    return shares.map(s=> ({...s, user:{ name:s.user.name}}));
 })
 
 module.exports.GetAll = ()=> {
-    var filteredShares = listWithOwner();
-    filterOutUserInfo(filteredShares);
-    return filteredShares;
+    let filteredShares = listWithOwner();
+    let mappedShares = filterOutUserInfo(filteredShares);
+    return mappedShares;
 }
 
 module.exports.GetWall = (handle)=> {
-    var filteredShares = listWithOwner().filter(share=> share.handle == handle);
-    filterOutUserInfo(filteredShares);
-    return filteredShares;
+    let filteredShares = listWithOwner().filter(share=> share.handle == handle);
+    let mappedShares = filterOutUserInfo(filteredShares);
+    return mappedShares;
     
 }
-
-module.exports.GetFeed = (handle)=> {
-    var filteredShares = listWithOwner().filter(share=> users.GetByHandle(handle).following).some(f=> f.handle == share.handle && f.isApproved) ;
-    filterOutUserInfo(filteredShares);
-    return filteredShares;
-}
-
-module.exports.Get = (share_id)=> shares_list[share_id];
 
 module.exports.Add = (share)=> {
     if (!share.handle) {
         throw { code: 422, msg: "share must have an Owner!" }
     }
+
+    share.id = shares_list[shares_list.length-1].id + 1;
     shares_list.push(share);
     
     return { ...share };
 }
 
-//TODO: decide if i actually want this
-module.exports.Update = (share_id, share)=> {
-    const oldObj = shares_list[share_id];
-    const newObj = {...oldObj, ...share }
-    shares_list[share_id] = newObj;
-    return newObj;
-}
-
 module.exports.Delete = (share_id)=> {
-    const share = shares_list[share_id];
-    shares_list.splice(share_id, 1);
-    return share;
+    //const share = shares_list[share_id];
+    location = shares_list.indexOf(shares_list.find(s=> s.id == share_id));
+    removedItem = shares_list.splice(location, 1);
+    return removedItem;
 }
