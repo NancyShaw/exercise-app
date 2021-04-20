8c9e7115e36412642e4e00e666328ea314eda7d9
Nancy Shaw <78286574+NancyShaw@users.noreply.github.com>
2021-04-20 16:30:51 -0400

    Setting auth header on calls from client to server

--- client/src/connections/myFetch.js
+++ client/src/connections/myFetch.js
@@ -2,24 +2,29 @@
  *  A wrapper for the native fetch API
  */
 
+import Session from "../models/Session";
  
 //const API_ROOT = process.env.VUE_APP_API_ROOT;
 const API_ROOT = "http://localhost:3000/"
 
  export function api(url, data) {
+    
     let promise;
+
+    const headers = { authorization: `bearer ${Session.token}` };
 
     if (data) {
         promise = fetch(API_ROOT + url, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             headers: {
+                ...headers,
               'Content-Type': 'application/json'
             },
             body: JSON.stringify(data) // body data type must match "Content-Type" header
           });
     } else {
-        promise = fetch(API_ROOT + url);
+        promise = fetch(API_ROOT + url, { headers });
     }
     return promise
         .then(x=> {
